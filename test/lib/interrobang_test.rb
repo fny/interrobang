95e1b277d47a28e0b68be1ce256966fbda8a9229
Faraz Yashar <faraz.yashar@gmail.com>
2015-04-03 15:22:35 -0400

    Rename block params to something more sensible

--- test/lib/interrobang_test.rb
+++ test/lib/interrobang_test.rb
@@ -29,61 +29,61 @@ describe Interrobang do
     '.bangify_class' =>
       -> (*args, &block) { Interrobang.bangify_class(*args, &block) }
   }
-  bangify_class_signatures.each do |method_call, method_block|
-    describe method_call do
+  bangify_class_signatures.each do |signature_name, bangify_class|
+    describe signature_name do
       it "converts all predicate? methods by default" do
-        method_block.call klass
+        bangify_class.call klass
         assert klass.new.true!
         assert klass.new.veritable!
       end
 
       it "returns an array of symbols of the bangified methods" do
-        assert_equal method_block.call(klass).sort, [:true!, :veritable!, :false!, :with_argument!].sort
+        assert_equal bangify_class.call(klass).sort, [:true!, :veritable!, :false!, :with_argument!].sort
       end
 
       it "converts all methods according to the provided prefix and suffix" do
-        method_block.call(klass, prefix: 'prefix_', suffix: '_suffix')
+        bangify_class.call(klass, prefix: 'prefix_', suffix: '_suffix')
         assert klass.new.prefix_true_suffix!
         assert klass.new.prefix_veritable_suffix!
       end
 
       it "converts all methods that match the provided pattern" do
-        method_block.call(klass, matching: %r{\Aso_.*\z})
+        bangify_class.call(klass, matching: %r{\Aso_.*\z})
         assert klass.new.so_true!
         assert klass.new.so_very_true!
         -> { klass.new.true! }.must_raise NoMethodError
       end
 
       it "converts all methods that match the provided pattern respected except" do
-        method_block.call(klass, matching: %r{\Aso_.*\z}, except: [:so_very_true])
+        bangify_class.call(klass, matching: %r{\Aso_.*\z}, except: [:so_very_true])
         assert klass.new.so_true!
         -> { klass.new.so_very_true! }.must_raise NoMethodError
         -> { klass.new.true! }.must_raise NoMethodError
       end
 
       it "except option accepts a singular symbol" do
-        method_block.call(klass, matching: %r{\Aso_.*\z}, except: :so_very_true)
+        bangify_class.call(klass, matching: %r{\Aso_.*\z}, except: :so_very_true)
         assert klass.new.so_true!
         -> { klass.new.so_very_true! }.must_raise NoMethodError
         -> { klass.new.true! }.must_raise NoMethodError
       end
 
       it "converts only the methods specified in the only option" do
-        method_block.call(klass, only: [:so_true])
+        bangify_class.call(klass, only: [:so_true])
         assert klass.new.so_true!
         -> { klass.new.so_very_true! }.must_raise NoMethodError
         -> { klass.new.true! }.must_raise NoMethodError
       end
 
       it "except option accepts a singular symbol" do
-        method_block.call(klass, only: :so_true)
+        bangify_class.call(klass, only: :so_true)
         assert klass.new.so_true!
         -> { klass.new.so_very_true! }.must_raise NoMethodError
         -> { klass.new.true! }.must_raise NoMethodError
       end
 
       it "converts only the methods specified in the only option with a block" do
-        method_block.call(klass, only: [:so_false]) do
+        bangify_class.call(klass, only: [:so_false]) do
           raise SomeError
         end
         -> { klass.new.so_false! }.must_raise SomeError
@@ -92,7 +92,7 @@ describe Interrobang do
       end
 
       it "performs the provided block for the bang method" do
-        method_block.call(klass) do
+        bangify_class.call(klass) do
           raise SomeError
         end
         assert klass.new.true!
@@ -100,7 +100,7 @@ describe Interrobang do
       end
 
       it "converts super methods when specified" do
-        method_block.call(klass, include_super: true, prefix: 'ensure_')
+        bangify_class.call(klass, include_super: true, prefix: 'ensure_')
         -> { klass.new.ensure_nil! }.must_raise Interrobang::FalsePredicate
       end
     end
@@ -118,54 +118,54 @@ describe Interrobang do
     '.bangify_method' =>
       -> (*args, &block) { Interrobang.bangify_method(*args, &block) }
   }
-  bangify_method_signatures.each do |method_call, method_block|
-    describe method_call do
+  bangify_method_signatures.each do |signature_name, bangify_method|
+    describe signature_name do
       describe "with a method that ends in a ?" do
         it "adds a ! method dropping the ?" do
-          method_block.call(klass, :true?)
+          bangify_method.call(klass, :true?)
           assert klass.new.true!
         end
 
         it "has no method missing shenanigans" do
-          method_block.call(klass, :true?)
+          bangify_method.call(klass, :true?)
           assert klass.new.respond_to?(:true!)
         end
       end
 
       describe "with a method that does not end in a ?" do
         it "adds a ! method" do
-          method_block.call(klass, :so_true)
+          bangify_method.call(klass, :so_true)
         end
       end
 
       it "returns the symbol of the bangified method" do
-        assert_equal method_block.call(klass, :true?), :true!
+        assert_equal bangify_method.call(klass, :true?), :true!
       end
 
       it "works on methods with arguments" do
-        method_block.call(klass, :with_argument?)
+        bangify_method.call(klass, :with_argument?)
         assert klass.new.with_argument!(true)
         -> { klass.new.with_argument!(false) }.must_raise Interrobang::FalsePredicate
       end
 
       it "does not convert assignment methods" do
-        method_block.call(klass, :assignment_=)
-        -> { klass.new.assignment_! }.must_raise NoMethodError
+        assert_equal bangify_method.call(klass, :assignment=), nil
+        assert !klass.new.respond_to?(:assignment!)
       end
 
       it "does not convert bang methods" do
-        method_block.call(klass, :bang!)
+        assert_equal bangify_method.call(klass, :bang!), nil
         assert_equal klass.new.bang!, '!'
       end
 
       it "converts undefined methods" do
-        method_block.call(klass, :is_not_defined)
+        bangify_method.call(klass, :is_not_defined)
         assert klass.new.respond_to?(:is_not_defined!)
       end
 
       describe "options" do
         it "adds any provided prefix or suffix to the bang method" do
-          method_block.call(klass, :true?, prefix: 'prefix_', suffix: '_suffix')
+          bangify_method.call(klass, :true?, prefix: 'prefix_', suffix: '_suffix')
           assert klass.new.prefix_true_suffix!
         end
       end
@@ -173,7 +173,7 @@ describe Interrobang do
       describe "falsey predicates" do
         describe "without a custom block" do
           it "raises a FalsePredicate error" do
-            method_block.call(klass, :false?)
+            bangify_method.call(klass, :false?)
             err = -> { klass.new.false! }.must_raise Interrobang::FalsePredicate
             assert_equal err.message, 'false? is false'
           end
@@ -181,14 +181,14 @@ describe Interrobang do
 
         describe "with a provided block" do
           it "performs the provided block for the bang method" do
-            method_block.call(klass, :false?) do
+            bangify_method.call(klass, :false?) do
               raise SomeError
             end
             -> { klass.new.false! }.must_raise SomeError
           end
 
           it "allows the provided block to take the predicate method as an argument" do
-            method_block.call(klass, :false?) do |predicate_method|
+            bangify_method.call(klass, :false?) do |predicate_method|
               raise SomeError, "#{predicate_method} isn't true"
             end
             err = -> { klass.new.false! }.must_raise SomeError
