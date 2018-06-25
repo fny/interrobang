9f5a1f83441397fce183d101935084a1b9b24f31
Faraz Yashar <faraz.yashar@gmail.com>
2018-06-25 12:20:46 -0400

    Remove code climate check

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,3 @@
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-end
-
 require 'minitest/autorun'
 require 'minitest/hell'
 require 'minitest/pride'
